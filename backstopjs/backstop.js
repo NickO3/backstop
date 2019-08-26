a3727523339b8abc53f9564d027971717da6b626
Nick Lewis <nick@o3world.com>
2019-08-26 12:46:34 -0400

    Adding node application

---
+++ backstopjs/backstop.js
@@ -0,0 +1,24 @@
+const _ = require('lodash');
+const request = require('request');
+const args = require('yargs').argv;
+const backstop = require('backstopjs');
+let myArgs = process.argv.slice(2);
+let commandToRun = myArgs[0];
+let scenarioName = myArgs[1];
+let queryParams = {};
+if (args.testUrl) {
+    queryParams.testUrl = args.testUrl;
+}
+if (args.referenceUrl) {
+    queryParams.referenceUrl = args.referenceUrl;
+}
+var jsonObject = request({ url: args.url + '/backstop/' + scenarioName, qs: queryParams},  function (error, response, body) {
+    if( "" !== commandToRun ) {
+        backstop(commandToRun, { config: JSON.parse(body)});
+    }
+    backstop("openReport", { config: JSON.parse(body)});
+});
+
+
+
+
